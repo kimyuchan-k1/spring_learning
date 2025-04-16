6a8258dbd4b7f067ffc594bddd7264f51913fba7
kimyuchan <rladbcks103@naver.com>
2025-04-16 13:46:04 +0900

    초기 데이터 sql 작성

---
+++ NPlusOne/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO TEAM (id, team_name) VALUES (1, 'TeamA');
+INSERT INTO TEAM (id, team_name) VALUES (2, 'TeamB');
+
+INSERT INTO MEMBER (id, name, team_id) VALUES (1, 'member1', 1);
+INSERT INTO MEMBER (id, name, team_id) VALUES (2, 'member2', 1);
+INSERT INTO MEMBER (id, name, team_id) VALUES (3, 'member3', 2);
+INSERT INTO MEMBER (id, name, team_id) VALUES (4, 'member4', 2);
+INSERT INTO MEMBER (id, name, team_id) VALUES (5, 'member5', 2);
