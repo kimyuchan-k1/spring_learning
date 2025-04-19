028e61999c0c0ed1c518a9bb6ae01fdd96d7f369
kimyuchan <rladbcks103@naver.com>
2025-04-19 17:49:46 +0900

    feat : 도메인 클래스 컨버터 적용 전 - MemberController

---
+++ SpringDataJpa/src/main/java/com/example/springdatajpa/controller/MemberController.java
@@ -0,0 +1,21 @@
+package com.example.springdatajpa.controller;
+
+import com.example.springdatajpa.entity.Member;
+import com.example.springdatajpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+}
