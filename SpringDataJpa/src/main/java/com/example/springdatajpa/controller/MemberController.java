1ac68f06e3514f03d8bda22008b86a08d6b9eb95
kimyuchan <rladbcks103@naver.com>
2025-04-19 18:05:26 +0900

    feat : Paging 구현하기 in Controller with PageableDefault

--- SpringDataJpa/src/main/java/com/example/springdatajpa/controller/MemberController.java
+++ SpringDataJpa/src/main/java/com/example/springdatajpa/controller/MemberController.java
@@ -3,9 +3,11 @@ package com.example.springdatajpa.controller;
 import com.example.springdatajpa.entity.Member;
 import com.example.springdatajpa.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,4 +25,23 @@ public class MemberController {
     public String findMember(@PathVariable("id") Member member) {
         return member.getUsername();
     }
+
+//    @GetMapping("/members")
+//    public Page<Member> list(Pageable pageable) {
+//        Page<Member> page = memberRepository.findAll(pageable);
+//        return page;
+//
+//    }
+
+
+
+    // http 요청 파라미터로 Pageable 을 넣을 수 있다.
+    // pageDefault 어노테이션 으로 페이징 기준의 기본값을 설정함.
+    @RequestMapping(value = "/members_page", method = RequestMethod.GET)
+    public String list(@PageableDefault(size = 12,sort = "username",
+    direction = Sort.Direction.DESC) Pageable pageable) {
+        return "Success";
+    }
+
+
 }
